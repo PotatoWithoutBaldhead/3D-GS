326dbfb24a06079e3b4d208344e1b6a36f76bd04
PotatoWithoutBaldhead <824537510@qq.com>
2024-08-06 10:11:05 +0800

    86

--- train.py
+++ train.py
@@ -61,15 +61,6 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
     first_iter += 1
     for iteration in range(first_iter, opt.iterations + 1):
-        if network_gui.conn == None:
-            network_gui.try_connect()
-        # while network_gui.conn != None:
-        #     try:
-        #         net_image_bytes = None
-        #         custom_cam, do_training, pipe.convert_SHs_python, pipe.compute_cov3D_python, keep_alive, scaling_modifer = network_gui.receive()
-        #         if custom_cam != None:
-        #             net_image = render(custom_cam, gaussians, pipe, background, scaling_modifer)["render"]
-        #             net_image_bytes = memoryview((torch.clamp(net_image, min=0, max=1.0) * 255).byte().permute(1, 2, 0).contiguous().cpu().numpy())
         iter_start.record()
         
         gaussians.update_learning_rate(iteration)
@@ -169,37 +160,6 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_i
         tb_writer.add_scalar('train_loss_patches/total_loss', loss.item(), iteration)
         tb_writer.add_scalar('iter_time', elapsed, iteration)
 
-    # # Report test and samples of training set
-    # if iteration in testing_iterations:
-    #     torch.cuda.empty_cache()
-    #     validation_configs = ({'name': 'test', 'cameras' : scene.getTestCameras()}, 
-    #                           {'name': 'train', 'cameras' : [scene.getTrainCameras()[idx % len(scene.getTrainCameras())] for idx in range(5, 30, 5)]})
-
-    #     for config in validation_configs:
-    #         if config['cameras'] and len(config['cameras']) > 0:
-    #             l1_test = 0.0
-    #             psnr_test = 0.0
-    #             for idx, viewpoint in enumerate(config['cameras']):
-    #                 image = torch.clamp(renderFunc(viewpoint, scene.gaussians, *renderArgs)["render"], 0.0, 1.0)
-    #                 gt_image = torch.clamp(viewpoint.original_image.to("cuda"), 0.0, 1.0)
-    #                 if tb_writer and (idx < 5):
-    #                     tb_writer.add_images(config['name'] + "_view_{}/render".format(viewpoint.image_name), image[None], global_step=iteration)
-    #                     if iteration == testing_iterations[0]:
-    #                         tb_writer.add_images(config['name'] + "_view_{}/ground_truth".format(viewpoint.image_name), gt_image[None], global_step=iteration)
-    #                 l1_test += l1_loss(image, gt_image).mean().double()
-    #                 psnr_test += psnr(image, gt_image).mean().double()
-    #             psnr_test /= len(config['cameras'])
-    #             l1_test /= len(config['cameras'])          
-    #             print("\n[ITER {}] Evaluating {}: L1 {} PSNR {}".format(iteration, config['name'], l1_test, psnr_test))
-    #             if tb_writer:
-    #                 tb_writer.add_scalar(config['name'] + '/loss_viewpoint - l1_loss', l1_test, iteration)
-    #                 tb_writer.add_scalar(config['name'] + '/loss_viewpoint - psnr', psnr_test, iteration)
-
-    #     if tb_writer:
-    #         tb_writer.add_histogram("scene/opacity_histogram", scene.gaussians.get_opacity, iteration)
-    #         tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
-    #     torch.cuda.empty_cache() 
-   
 if __name__ == "__main__":
     # Set up command line argument parser 设置命令行参数解释器
     parser = ArgumentParser(description = "Train script parameters")  # 设置命令行参数解析器 parser
